284f85990670d3b38c6dbbd7f1b2f32b59352a83
Ryan Haidy Papudi <ryanhaidypapudi@gmail.com>
2020-10-03 20:59:24 +0700

    feat: add stat to users api

--- app/Services/UserService/GetUser.php
+++ app/Services/UserService/GetUser.php
@@ -5,23 +5,39 @@ namespace App\Services\UserService;
 use App\Models\User;
 use App\Services\ServiceInterface;
 use App\Services\DefaultService;
+use Carbon\Carbon;
 
 class GetUser extends DefaultService implements ServiceInterface
 {
     public function process($dto)
     {
-        $user = User::orderBy($dto['sort_by'], $dto['sort_dir']);
+        $query = User::orderBy($dto['sort_by'], $dto['sort_dir']);
 
         if ($dto['id'] != null) {
-            $user->where('id', $dto['id']);
-            $data = $user->first();
+            $query->where('id', $dto['id']);
+            $data = $this->convert($query->first());
         } else {
-            $this->results['pagination'] = $this->paginationDetail($dto['per_page'], $dto['page'], $user->count());
-            $user = $this->paginateData($user, $dto['per_page'], $dto['page']);
-            $data = $user->get();
+            $this->results['pagination'] = $this->paginationDetail($dto['per_page'], $dto['page'], $query->count());
+            $query = $this->paginateData($query, $dto['per_page'], $dto['page']);
+            $data = $query->get()->map(function ($query) {
+                return $this->convert($query);
+            });
         }
 
         $this->results['message'] = 'User data successfully fetched';
         $this->results['data'] = $data;
+    }
+    
+    private function convert($user)
+    {
+        $response = $user;
+
+        $response->total_check_in_topics = $user->checkInTopics->count();
+        $response->total_days = Carbon::now()
+            ->startOfDay()
+            ->diffInDays(carbon::parse($user->created_at)->startOfDay()) + 1;
+        unset($user->checkInTopics);
+        
+        return $response;
     }
 }
