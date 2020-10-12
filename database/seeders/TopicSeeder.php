6c3106242226cf20db373a05a5d57ab9923c9768
Ryan Haidy Papudi <ryanhaidypapudi@gmail.com>
2020-10-12 11:12:53 +0700

    fix: wrong category id

--- database/seeders/TopicSeeder.php
+++ database/seeders/TopicSeeder.php
@@ -16,15 +16,15 @@ class TopicSeeder extends Seeder
     public function run()
     {
         $topics = [
-            [ 'id' => 1, 'category_id' => 4, 'user_id' => 1, 'name' => 'Drink More Water', 'slug' => 'drink-more-water', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
-            [ 'id' => 2, 'category_id' => 4, 'user_id' => 1, 'name' => 'Exercise', 'slug' => 'exercise', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
-            [ 'id' => 3, 'category_id' => 4, 'user_id' => 1, 'name' => 'Read', 'slug' => 'read', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
-            [ 'id' => 4, 'category_id' => 5, 'user_id' => 1, 'name' => 'Exercise', 'slug' => 'exercise-1', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
-            [ 'id' => 5, 'category_id' => 5, 'user_id' => 1, 'name' => 'Run', 'slug' => 'run', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
-            [ 'id' => 6, 'category_id' => 5, 'user_id' => 1, 'name' => 'Go to Gym', 'slug' => 'go-to-gym', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
-            [ 'id' => 7, 'category_id' => 6, 'user_id' => 1, 'name' => 'Set Priorities For Your Day', 'slug' => 'set-priorities-for-your-day', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
-            [ 'id' => 8, 'category_id' => 6, 'user_id' => 1, 'name' => 'Write To Do List', 'slug' => 'write-to-do-list', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
-            [ 'id' => 9, 'category_id' => 6, 'user_id' => 1, 'name' => 'Inbox Zero', 'slug' => 'inbox-zero', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
+            [ 'id' => 1, 'category_id' => 1, 'user_id' => 1, 'name' => 'Drink More Water', 'slug' => 'drink-more-water', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
+            [ 'id' => 2, 'category_id' => 1, 'user_id' => 1, 'name' => 'Exercise', 'slug' => 'exercise', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
+            [ 'id' => 3, 'category_id' => 1, 'user_id' => 1, 'name' => 'Read', 'slug' => 'read', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
+            [ 'id' => 4, 'category_id' => 2, 'user_id' => 1, 'name' => 'Exercise', 'slug' => 'exercise-1', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
+            [ 'id' => 5, 'category_id' => 2, 'user_id' => 1, 'name' => 'Run', 'slug' => 'run', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
+            [ 'id' => 6, 'category_id' => 2, 'user_id' => 1, 'name' => 'Go to Gym', 'slug' => 'go-to-gym', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
+            [ 'id' => 7, 'category_id' => 3, 'user_id' => 1, 'name' => 'Set Priorities For Your Day', 'slug' => 'set-priorities-for-your-day', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
+            [ 'id' => 8, 'category_id' => 3, 'user_id' => 1, 'name' => 'Write To Do List', 'slug' => 'write-to-do-list', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
+            [ 'id' => 9, 'category_id' => 3, 'user_id' => 1, 'name' => 'Inbox Zero', 'slug' => 'inbox-zero', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'deleted_at' => null, 'background' => null ],
         ];
 
         foreach ($topics as $topic) {
