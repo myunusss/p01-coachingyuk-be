219a428ce7becb730df9bf18419d654f4fb8e3fb
Ryan Haidy Papudi <ryanhaidypapudi@gmail.com>
2020-10-31 12:53:37 +0700

    feat: update email template

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -1,8 +1,19 @@
+
+@slot('header')
+@component('mail::header', ['url' => config('app.url')])
+<img src="http://coachingyuk.com/favicon.ico" alt="Coachingyuk"/>
+@endcomponent
+@endslot
+
 @component('mail::message')
 # Account Created
 
-Hi {{ $user->first_name }}
-Your account has successfully been created!
+Dear {{ $user->first_name }},<br>
+Thank you for registering for Coachingyuk, a coaching platform to accelerate your growth in career, business, and life. Please find below your registration information:<br>
+<br>
+Username: {{ $user->username }}<br>
+<br>
+Click on the button below to go to your account. If you have any questions, please feel free to contact support@coachingyuk.com.
 
 @component('mail::button', ['url' => $url, 'color' => 'primary'])
 Verify Account
